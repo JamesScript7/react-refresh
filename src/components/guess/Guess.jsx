6b1391e79e0ddb88e49542564b28502c762ba016
James Kim <yameskim@gmail.com>
2018-06-20 13:10:57 -0400

    Update syntax, let to const, deconstruct state

--- src/components/guess/Guess.jsx
+++ src/components/guess/Guess.jsx
@@ -19,27 +19,19 @@ class Guess extends Component {
     this.onNumberChange = this.onNumberChange.bind(this);
     this.onRangeChange = this.onRangeChange.bind(this);
     this.resetGame = this.resetGame.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   pickNumber(x) {
-    let d = new Date();
-    let newNum = (d.getMilliseconds() % x - 1) + 1
-    return newNum ? newNum: newNum + 1;
+    const d = new Date();
+    const newNum = (d.getMilliseconds() % x - 1) + 1;
+    return newNum ? newNum : newNum + 1;
   }
-
-  onSubmit(e) {
+  handleSubmit(e) {
     e.preventDefault();
+    let { max, lives, status, active } = this.state;
 
-    if (this.state.status === 'won' || this.state.lives === 0) {
-      return;
-    }
-
-    let
-      max = this.state.max,
-      lives = this.state.lives,
-      active = this.state.active,
-      status, message;
+    if (status === 'won' || lives === 0) return;
+    let message;
 
     if (lives === 1 && !(this.guess === this.answer)) {
       status = 'wrong';
@@ -53,11 +45,11 @@ class Guess extends Component {
     } else if (this.guess > this.answer) {
       status = 'wrong';
       message = 'You guessed too high!';
-      lives = this.state.lives - 1;
+      lives = lives - 1;
     } else if (this.guess < this.answer) {
       status = 'wrong';
       message = 'You guessed too low!';
-      lives = this.state.lives - 1;
+      lives = lives - 1;
     } else {
       status = 'wrong';
       message = '';
@@ -69,24 +61,21 @@ class Guess extends Component {
       status,
       message,
       active,
-    })
+    });
 
     e.target.guess.value = '';
   }
-
   onNumberChange(e) {
     e.preventDefault();
-
-    let userGuess = e.target.value;
+    const userGuess = e.target.value;
 
     this.guess = parseInt(userGuess, 10);
   }
-
   onRangeChange(e) {
     e.target.min = 5;
     e.target.max = 1000;
     let range = e.target.value;
-    console.log(range);
+    // console.log(range);
 
     this.setState({
       max: range,
@@ -95,9 +84,8 @@ class Guess extends Component {
 
     this.answer = this.pickNumber(range);
   }
-
   resetGame() {
-    let reset = {
+    const reset = {
       max: 10,
       lives: Math.round(Math.log2(7)),
       status: 'new',
@@ -108,34 +96,35 @@ class Guess extends Component {
     this.answer = this.pickNumber(this.state.max);
     this.setState(reset);
   }
-
   render() {
+    const { max, lives, status, message, active } = this.state;
+
     return (
       <div className="guess">
         <h1>Guess the Number!</h1>
         <div className="top">
-          <div>Lives left: {this.state.lives}</div>
-          <button className={`reset-btn ${this.state.active ? "hide": "active"}`} onClick={this.resetGame}>Reset Game</button>
+          <div>Lives left: {lives}</div>
+          <button className={`reset-btn ${active ? "hide": "active"}`} onClick={this.resetGame}>Reset Game</button>
         </div>
 
-        <form className="guess" onSubmit={this.onSubmit}>
+        <form className="guess" onSubmit={this.handleSubmit}>
           <div>Number Range:</div>
-          <input type="range" value={this.state.max} name="range" onChange={this.onRangeChange} />
+          <input type="range" value={max} name="range" onChange={this.onRangeChange} />
 
           <div className="center">
-            <div className="guess-it">Guess the number between 1 and {this.state.max}</div>
+            <div className="guess-it">Guess the number between 1 and {max}</div>
             <div>
               <label>
                 <input type="number" name="guess" onChange={this.onNumberChange} />
               </label>
-              <button className={`enter-btn ${this.state.active ? "active": "hide"}`}>Enter</button>
+              <button className={`enter-btn ${active ? "active": "hide"}`}>Enter</button>
             </div>
           </div>
 
         </form>
         <div className="msg">
-          <div className={this.state.status}>{this.state.message}</div>
-          <div>{!this.state.active ? `The magic number was ${this.answer}` : ""}</div>
+          <div className={status}>{message}</div>
+          <div>{!active ? `The magic number was ${this.answer}` : ""}</div>
         </div>
       </div>
     )
